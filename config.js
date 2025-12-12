fd649611097ae747a8ddb65eb8b6e11d7b491fdf
ronaldinhogostosinho <168553640+ronaldsktt@users.noreply.github.com>
2025-12-12 14:59:35 -0300

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ const PRODUTOS = {
     nome: 'deolne',
     bot_url: 'https://t.me/Mirella_bot?start=adsmirella',
     grupo_vip: 'https://t.me/+LINK_GRUPO_MIRELLA',
-    foto: '.imagens/monkey.jpg',
+    foto: './imagens/monkey.jpg',
     cor_primaria: '#ff6b9d',
     cor_secundaria: '#c44569'
   },
